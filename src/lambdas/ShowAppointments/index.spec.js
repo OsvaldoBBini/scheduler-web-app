4213002d1c7ba2d32b96c174fca52bfef554e7cd
Osvaldo <osvaldobbini@gmail.com>
2024-08-10 09:20:45 -0300

    try up tests

--- src/lambdas/ShowAppointments/index.spec.js
+++ src/lambdas/ShowAppointments/index.spec.js
@@ -3,11 +3,11 @@ import { handler } from './index.mjs';
 
 describe('show', () => {
 
-  it('Should be able to show 2 appointments', async () => {
+  it('Should be able to show 1 appointment', async () => {
 
     const event = {
       queryStringParameters: {
-        userId: '123456',
+        userId: '1234567',
         appointmentDate: '23/05/2050'
       }
     };
@@ -15,7 +15,7 @@ describe('show', () => {
     const { body } = await handler(event);
     const appointments = JSON.parse(body);
 
-    expect(appointments.Count).toBe(2);
+    expect(appointments.Count).toBe(1);
   });
 
 });
