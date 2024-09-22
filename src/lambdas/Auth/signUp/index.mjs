7deadc57a90d8569acdc177374be3560bfd654bf
Osvaldo <osvaldobbini@gmail.com>
2024-09-21 22:06:57 -0300

    fix: import path

--- src/lambdas/Auth/signUp/index.mjs
+++ src/lambdas/Auth/signUp/index.mjs
@@ -1,4 +1,4 @@
-import { clients } from "../../lib/Clients.mjs";
+import clients from '../../../lib/Clients.mjs'
 import { SignUpCommand, UsernameExistsException } from "@aws-sdk/client-cognito-identity-provider";
 
 export async function handler(event) {
